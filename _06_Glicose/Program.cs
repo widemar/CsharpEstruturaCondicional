544784616083bda017ea1ebf8ba1e5eee8063e16
widemar <widemarinternship@gmail.com>
2022-07-03 13:24:15 -0300

    C# Exercícios

---
+++ _06_Glicose/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Digite a medida da glicose: ");
+double glicose = double.Parse(Console.ReadLine()!, info);
+
+string classificacao = glicose switch
+{
+    <= 100 => "normal",
+    <= 140 => "elevado",
+    _ => "diabetes"
+};
+
+Console.WriteLine($"Classificação: {classificacao}");
+
