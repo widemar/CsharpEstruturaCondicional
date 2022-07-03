ad93b1f5722c70a8c0257020b71482eae96bd707
widemar <widemarinternship@gmail.com>
2022-07-03 11:47:12 -0300

    C# Exercícios

---
+++ _02_Bhaskara/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Coeficiente A: ");
+double a = double.Parse(Console.ReadLine()!, info);
+Console.Write("Coeficiente B: ");
+double b = double.Parse(Console.ReadLine()!, info);
+Console.Write("Coeficiente C: ");
+double c = double.Parse(Console.ReadLine()!, info);
+
+double delta = Math.Pow(b, 2) - 4 * a * c;
+double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+if (delta < 0)
+{
+    Console.WriteLine("Está equação não possui raízes reais");
+}
+else
+{
+    Console.WriteLine($"X1 = {x1.ToString("F4", info)}");
+    Console.WriteLine($"X2 = {x2.ToString("F4", info)}");
+}
---
+++ _03_MenorDeTres/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Primeiro valor: ");
+int primeiro = int.Parse(Console.ReadLine()!);
+Console.Write("Segundo valor: ");
+int segundo = int.Parse(Console.ReadLine()!);
+Console.Write("Terceiro valor: ");
+int terceiro = int.Parse(Console.ReadLine()!);
+
+int menor;
+menor = primeiro < segundo && primeiro < terceiro ? primeiro : Math.Min(segundo, terceiro);
+
+Console.WriteLine($"MENOR = {menor}");
