46522005e757808011e82df6990143ed50c9f0d4
widemar <widemarinternship@gmail.com>
2022-07-03 11:20:55 -0300

    C# Exercícios

--- _01_Notas/Program.cs
+++ _01_Notas/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Globalization;
 
-Console.WriteLine("Hello, World!");
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Digite a primeira nota: ");
+double notaUm = double.Parse(Console.ReadLine()!, info);
+Console.Write("Digite a segunda nota: ");
+double notaDois = double.Parse(Console.ReadLine()!, info);
+
+double somaNotas = notaUm + notaDois;
+
+Console.WriteLine($"NOTA FINAL = {somaNotas.ToString("F1", info)}");
+if (somaNotas < 60)
+{
+    Console.WriteLine("REPROVADO");
+}
