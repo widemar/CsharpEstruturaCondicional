c774cb0d7d9a1b269b135f8548fa0cb428cdef27
widemar <widemarinternship@gmail.com>
2022-07-03 13:23:44 -0300

    C# Exercícios

---
+++ _04_Operadora/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Digite a quantidade de minutos: ");
+int minutos = int.Parse(Console.ReadLine()!);
+
+double valorPagar = minutos <= 100 ? 50.00 : (minutos - 100) * 2 + 50.00;
+
+Console.WriteLine($"Valor a pagar: R${valorPagar.ToString("F2", info)}");
---
+++ _05_TrocoVerificado/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Preço unitário do produto: ");
+double precoUnitario = double.Parse(Console.ReadLine()!, info);
+Console.Write("Quantidade comprada: ");
+int quantidade = int.Parse(Console.ReadLine()!);
+Console.Write("Dinheiro recebido: ");
+double dinheiroRecebido = double.Parse(Console.ReadLine()!, info);
+
+double valorDaCompra = precoUnitario * quantidade;
+
+if (dinheiroRecebido < valorDaCompra)
+{
+    double dinheiroQueFalta = valorDaCompra - dinheiroRecebido;
+    Console.WriteLine($"DINHEIRO INSUFICIENTE. FALTAM {dinheiroQueFalta.ToString("F2", info)} REAIS.");
+}
+else
+{
+    double troco = dinheiroRecebido - valorDaCompra;
+    Console.WriteLine($"TROCO = {troco.ToString("F2", info)}");
+}
