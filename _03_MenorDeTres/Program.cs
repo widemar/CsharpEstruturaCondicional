bad3730b794a5b66c4eb4974f584e066db2c1ea7
widemar <widemarinternship@gmail.com>
2022-07-03 11:48:37 -0300

    C# Exercícios

--- _03_MenorDeTres/Program.cs
+++ _03_MenorDeTres/Program.cs
@@ -5,7 +5,6 @@ int segundo = int.Parse(Console.ReadLine()!);
 Console.Write("Terceiro valor: ");
 int terceiro = int.Parse(Console.ReadLine()!);
 
-int menor;
-menor = primeiro < segundo && primeiro < terceiro ? primeiro : Math.Min(segundo, terceiro);
+int menor = primeiro < segundo && primeiro < terceiro ? primeiro : Math.Min(segundo, terceiro);
 
 Console.WriteLine($"MENOR = {menor}");
