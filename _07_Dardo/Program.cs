b8fe934114dfc655956d18db819bb66ac24de01c
widemar <widemarinternship@gmail.com>
2022-07-03 13:51:48 -0300

    C# Exercícios

---
+++ _07_Dardo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.WriteLine("Digite as três distâncias: ");
+double distanciaUm = double.Parse(Console.ReadLine()!, info);
+double distanciaDois = double.Parse(Console.ReadLine()!, info);
+double distanciaTres = double.Parse(Console.ReadLine()!, info);
+
+double maiorDistancia = distanciaUm > distanciaDois && distanciaUm > distanciaTres
+    ? distanciaUm
+    : Math.Max(distanciaDois, distanciaTres);
+
+Console.WriteLine($"MAIOR DISTÂNCIA = {maiorDistancia.ToString("F2", info)}");
---
+++ _08_Temperatura/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Você vai digitar a temperatura em qual escala? C ou F: ");
+char tipoTemperatura = char.Parse(Console.ReadLine()!.ToUpper());
+
+if (tipoTemperatura == 'F')
+{
+    Console.Write("Digite a temperatura em Fahrenheit: ");
+    double fahrenheit = double.Parse(Console.ReadLine()!, info);
+    double conversao = (fahrenheit - 32) / 1.8;
+    Console.WriteLine($"Temperatura equivalente em Celsius: {conversao.ToString("F2", info)}");
+}
+else
+{
+    Console.Write("Digite a temperatura em Celsius: ");
+    double celsius = double.Parse(Console.ReadLine()!, info);
+    double conversao = celsius * 1.8 + 32;
+    Console.WriteLine($"Temperatura equivalente em Fahrenheit: {conversao.ToString("F2", info)}");
+}
