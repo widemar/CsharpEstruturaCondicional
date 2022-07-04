81bc64fff228b8f1c24f5284d247aba14614b6c3
widemar <widemarinternship@gmail.com>
2022-07-03 21:12:03 -0300

    C# Exercícios

---
+++ _12_TempoDeJogo/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Hora Inicial: ");
+int horaInicial = int.Parse(Console.ReadLine()!);
+Console.Write("Hora Final: ");
+int horaFinal = int.Parse(Console.ReadLine()!);
+
+int duracao;
+if (horaInicial > horaFinal)
+{
+    duracao = 24 - (horaInicial - horaFinal);
+}
+else if (horaFinal > horaInicial)
+{
+    duracao = horaFinal - horaInicial;
+}
+else
+{
+    duracao = 24 - horaInicial - horaFinal;
+}
+
+Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
